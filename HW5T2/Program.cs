d36c2cb123e06c80b9722b2bbbab1162812dd3ae
Anna Nikolenko <nikolenko_anmai@mail.ru>
2023-02-27 21:44:31 +0300

    Task 2

---
+++ HW5T2/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GenerateArray(int length, int minValue, int maxValue)
+{
+    int[] result = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int oddPosition(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+int size = 4;
+int minValue = 1;
+int maxValue = 99;
+int[] myArray = GenerateArray(size, minValue, maxValue);
+ShowArray(myArray);
+System.Console.WriteLine($"Сумма элементов на нечетных позициях = {oddPosition(myArray)}");
+
